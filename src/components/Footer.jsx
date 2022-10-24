6fac392bf173a6dae7a35c099cafb81869ae94ce
Tomoko-hub <haisaitomoco@gmail.com>
2022-10-24 18:09:04 +0300

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+
+  return (
+    <div>
+        <footer className="container">
+        <p>© Tomoko Uehara 2022</p>
+        </footer>
+    </div>
+    )
+}
+
+export default Footer
